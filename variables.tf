1fd91a093b38882ede24b4a9c67db9aafbab58fe
junaid-13 <zufishanjunaid.1999@gmail.com>
2025-09-03 13:36:21 +0530

    Added the initial code for variables.tf file.

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "github_token" {
+  type = string
+  sensitive = true
+  }
+
+variable "github_owner" {
+    type = string
+    default = "junaid-13"
+}
+
+variable "repositories" { 
+    type = list(string)
+}
